70e91b81b1c2abbf9662b3b69840ac07f94175a2
MRaehanR <raehanrobban2@gmail.com>
2025-12-23 13:15:43 +0700

    fix: seeder priority

---
+++ app/db/seeds/card_market.py
@@ -0,0 +1,19 @@
+from flask_seeder import Seeder, Faker, generator
+from app.db.models.card_market import CardMarket
+from app.db.models.card_user import CardUser
+from random import randint
+
+class CardMarketSeeder(Seeder):
+    def __init__(self):
+        self.priority = 4
+        
+    def run(self):
+        cardUser1 = CardUser.query.filter_by(user_id=1).all()
+        
+        for card_market in cardUser1:
+            cardUserSells = CardMarket(
+                seller_id=1,
+                card_id=card_market.card_id,
+                price=randint(10, 1000)
+            )
+            self.db.session.add(cardUserSells)
